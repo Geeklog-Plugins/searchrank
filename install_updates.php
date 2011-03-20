ef7520eb8f2b0e4af598d6710c0786d556eb9841
Rouslan Placella <rouslan@placella.com>
2011-03-20 23:57:42 +0000

    more upgrade fixes

--- install_updates.php
+++ install_updates.php
@@ -13,6 +13,8 @@ function SEARCHRANK_update_ConfValues_1_0_0()
     $c->add('itemsperpage', 20, 'text', 0, 0, null, 60, true, $me, 0);
     $c->add('blockorder', 1, 'text', 0, 0, null, 25, true, $me, 0);
     $c->add('rootonly', false, 'select', 0, 0, 1, 20, true, $me, 0);
+
+	return true;
 }
 
 ?>
